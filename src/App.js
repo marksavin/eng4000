808992163e41c8a2e82ef769fdc177380bea61d5
wmaneesh <36091416+wmaneesh@users.noreply.github.com+wmaneesh@outlook.com>
2020-11-12 11:07:56 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@
 import React from 'react';
 import './App.css';
 
-//I DONT LIKE MEN MARK
-//Maneesh, who you lying too, us or yourself? Behonest, we'll support you!  
+
 function App() {
   return (
     <div className="App">
-
+      <Navigation/>
     </div>
   );
 }
