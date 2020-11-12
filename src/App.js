7e05b5616434c954735b590c4d748c956a2f1ef8
wmaneesh <36091416+wmaneesh@users.noreply.github.com+wmaneesh@outlook.com>
2020-11-12 10:52:35 -0500

    Removed stock logo and added navbar template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //testing
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //I DONT LIKE MEN MARK
@@ -8,20 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = ({ }) => {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+
+      </nav>
+    );
+}
+
+export default NavBar;
