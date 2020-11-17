b60d6f3a2c7f5ebdb1d293d759b19bc53b9ed672
wmaneesh <36091416+wmaneesh@users.noreply.github.com+wmaneesh@outlook.com>
2020-11-17 18:21:08 -0500

    added some new modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //testing
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Navigation from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
+      <Navigation />
     </div>
   );
 }
