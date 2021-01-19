6e1fd4334cad449054b124c62111bb49f0a4537d
brandon197 <pinkzepp@my.yorku.ca>
2021-01-19 18:55:24 -0500

    Switched to Material UI textfields

--- eng4k-frontend/src/Components/CreatePatient/CreatePatient.js
+++ eng4k-frontend/src/Components/CreatePatient/CreatePatient.js
@@ -1,20 +1,21 @@
 import React from "react";
 //import DateFnsUtils from "@date-io/date-fns";
 //import { DatePicker } from "@material-ui/pickers";
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, TextField } from "@material-ui/core";
 
 class CreatePatient extends React.Component {
   state = {
-    id: 0,
+    //id: 0,
     fname: "",
     lname: "",
-    adDate: "", // have a function to get current date
+    //adDate: "", // have a function to get current date
     dOBDay: "",
     dOBMonth: "",
     dOBYear: "",
     weight: 0.0,
     height: 0.0,
   };
+
   componentDidUpdate() {
     console.log(this.state);
   }
@@ -29,71 +30,77 @@ class CreatePatient extends React.Component {
           <div className="patientForm">
             <div className="cNames">
               <div className="FirstName">
-                <label className="fnameLabel" htmlFor="firstName">
-                  First Name:
-                </label>
-                <input
-                  className="fname"
-                  type="text"
-                  onChange={(e) => this.setState({ fname: e.target.value })}
-                />
+                <form className="fname" noValidate autoComplete="off">
+                  <TextField
+                    id="fnameField"
+                    label="First Name"
+                    variant="outlined"
+                    onChange={(e) => this.setState({ fname: e.target.value })}
+                  />
+                </form>
               </div>
               <div className="LastName">
-                <label className="lNameLabel" htmlFor="lastName">
-                  Last Name:
-                </label>
-                <input
-                  className="lname"
-                  type="text"
-                  onChange={(e) => this.setState({ lname: e.target.value })}
-                />
+                <form className="lname" noValidate autoComplete="off">
+                  <TextField
+                    id="lnameField"
+                    label="Last Name"
+                    variant="outlined"
+                    onChange={(e) => this.setState({ lname: e.target.value })}
+                  />
+                </form>
               </div>
-              <div className="Dob">
-                <label className="doblabel" htmlFor="dob">
-                  Date of Birth:
-                </label>
-                <input
-                  className="dobMonth"
-                  type="text"
-                  placeholder="mm"
+            </div>
+            <div className="Dob">
+              <label className="doblabel" htmlFor="dob">
+                Date of Birth:
+              </label>
+              <form className="dobmonth" noValidate autoComplete="off">
+                <TextField
+                  id="dobMonthField"
+                  label="mm"
+                  variant="outlined"
                   onChange={(e) => this.setState({ dOBMonth: e.target.value })}
                 />
-                -
-                <input
-                  className="dobday"
-                  type="text"
-                  placeholder="dd"
+              </form>
+              -
+              <form className="dobday" noValidate autoComplete="off">
+                <TextField
+                  id="dobDayField"
+                  label="dd"
+                  variant="outlined"
                   onChange={(e) => this.setState({ dOBDay: e.target.value })}
                 />
-                -
-                <input
-                  className="dobyear"
-                  type="text"
-                  placeholder="yyyy"
+              </form>
+              -
+              <form className="dobyear" noValidate autoComplete="off">
+                <TextField
+                  id="dobYearField"
+                  label="yyyy"
+                  variant="outlined"
                   onChange={(e) => this.setState({ dOBYear: e.target.value })}
                 />
-              </div>
+              </form>
+            </div>
+            <div className="hW">
               <div className="Weight">
-                <label className="weightLabel" htmlFor="weight">
-                  Weight:
-                </label>
-                <input
-                  className="weight"
-                  type="text"
-                  onChange={(e) => this.setState({ weight: e.target.value })}
-                />{" "}
-                lbs
+                <form className="weightForm" noValidate autoComplete="off">
+                  <TextField
+                    id="weightField"
+                    label="Weight (lbs)"
+                    variant="outlined"
+                    onChange={(e) => this.setState({ weight: e.target.value })}
+                  />
+                </form>
               </div>
               <div className="Height">
-                <label className="heightLabel" htmlFor="height">
-                  Height:
-                </label>
-                <input
-                  className="height"
-                  type="text"
-                  onChange={(e) => this.setState({ height: e.target.value })}
-                ></input>
-                m
+                <form className="heightForm" noValidate autoComplete="off">
+                  <TextField
+                    id="heightField"
+                    label="Height (m)"
+                    variant="outlined"
+                    onChange={(e) => this.setState({ height: e.target.value })}
+                  />
+                </form>
               </div>
             </div>
           </div>
