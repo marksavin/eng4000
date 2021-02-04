f355aeb71a1d627bf75712e4ba8665555033d502
brandon197 <brandonchan.bnc@gmail.com>
2021-02-04 18:46:34 -0500

    ...

--- eng4k-frontend/src/index.js
+++ eng4k-frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "semantic-ui-css/semantic.min.css";
 // import NursePage from "./Components/Nurse/NursePage.js";
 
 ReactDOM.render(
