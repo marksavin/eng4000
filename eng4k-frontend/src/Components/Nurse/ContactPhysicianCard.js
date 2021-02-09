b60cf7d351865651dbc441cf5e3eb6bf05cb8e3f
brandon197 <brandonchan.bnc@gmail.com>
2021-02-08 22:46:22 -0500

    Added text field o card

--- eng4k-frontend/src/Components/Nurse/ContactPhysicianCard.js
+++ eng4k-frontend/src/Components/Nurse/ContactPhysicianCard.js
@@ -1,26 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "semantic-ui-css/semantic.min.css";
+import { TextField, Button, Collapse, IconButton } from "@material-ui/core";
+import ArrowDropDownCircleIcon from "@material-ui/icons/ArrowDropDownCircle";
 
-const ContactPhysicanCard = () => {
+const ContactPhysicanCard = (props) => {
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleChange = (e) => {
+    setRemarks(e.target.value);
+    console.log(remarks);
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const [remarks, setRemarks] = useState("");
+
   return (
-    <div className="container">
-      <div class="ui card">
-        <div class="image">
+    <div className="contactPcontainer">
+      <div className="ui card">
+        <div className="image">
           <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" />
         </div>
-        <div class="content">
-          <div class="header">Matthew</div>
-          <div class="meta">
-            <span class="date">Joined in 2015</span>
+
+        <div className="content">
+          <div className="header">{props.pname}</div>
+          <div className="meta">
+            <span className="date">{props.specialty}</span>
           </div>
-          <div class="description">
-            Matthew is a musician living in Nashville.
+          <div className="description">{props.availability}</div>
+          <div className="controlButton">
+            <Collapse in={!expanded}>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={handleExpandClick}
+              >
+                Request Contact
+              </Button>
+            </Collapse>
           </div>
         </div>
-        <div class="extra content">
-          <a>
-            <i aria-hidden="true" class="user icon"></i>22 Friends
-          </a>
+
+        <div className="extraContent">
+          <Collapse in={expanded}>
+            <form className="remarksForm">
+              <div className="remarksText">
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Additional Remarks"
+                  multiline
+                  rows={2}
+                  variant="outlined"
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="ContactButtons">
+                <div className="ContactCancelButton">
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleExpandClick}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+                <div className="ContactSubmitButton">
+                  <Button variant="contained" color="primary">
+                    Submit
+                  </Button>
+                </div>
+              </div>
+            </form>
+          </Collapse>
         </div>
       </div>
     </div>
