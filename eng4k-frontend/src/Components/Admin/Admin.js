c29b42aed797ca9f76c85ad90b9b1a077a1211b5
marksavin <mark831@my.yorku.ca>
2021-02-08 20:04:55 -0500

    New Admin Page!

---
+++ eng4k-frontend/src/Components/Admin/Admin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Admin extends React.Componet {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+        <p>hi</p>
+      </div>
+    );
+  }
+}
+
+export default Admin;
