71b57f6e10226ca87cdaa1027f55a25161829cb0
marksavin <mark831@my.yorku.ca>
2021-02-10 22:40:11 -0500

    New Form for Create Patient.

--- eng4k-frontend/src/Components/CreatePatient/CreatePatient.js
+++ eng4k-frontend/src/Components/CreatePatient/CreatePatient.js
@@ -2,6 +2,16 @@ import React from "react";
 //import DateFnsUtils from "@date-io/date-fns";
 //import { DatePicker } from "@material-ui/pickers";
 import { Button, TextField } from "@material-ui/core";
+import * as yup from "yup";
+
+// a schema is just a place to define a set of rules
+const reviewSchema = yup.object({
+  // here we create a yup object where we will define our schema
+  fName: yup.string().required(),
+  lName: yup.string().required(),
+  weight: yup.number().required,
+  height: yup.number().required,
+});
 
 class CreatePatient extends React.Component {
   state = {
@@ -9,9 +19,8 @@ class CreatePatient extends React.Component {
     fname: "",
     lname: "",
     //adDate: "", // have a function to get current date
-    dOBDay: "",
-    dOBMonth: "",
-    dOBYear: "",
+    admissionDate: "",
+    dateOfBirth: "",
     weight: 0.0,
     height: 0.0,
   };
@@ -21,7 +30,9 @@ class CreatePatient extends React.Component {
   }
 
   handleSubmit = (event) => {
+    // api call
     event.preventDefault();
+    console.log(this.state);
     fetch(`/nurse/addNewPatient`, {
       method: "POST",
       headers: {
@@ -38,48 +49,75 @@ class CreatePatient extends React.Component {
 
   render() {
     return (
-      <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+      <form
+        noValidate
+        autoComplete="off"
+        validationschema={reviewSchema}
+        onSubmit={this.handleSubmit}
+      >
         <div className="container-CreatePatient">
           <div className="promptMessage">
             Please Enter the Following Information:
           </div>
           <div className="patientForm">
             <div className="cNames">
-              <div className="FirstName">
-                <div className="fname" noValidate autoComplete="off">
-                  <TextField
-                    id="fnameField"
-                    label="First Name"
-                    variant="outlined"
-                    onChange={(e) => this.setState({ fname: e.target.value })}
-                  />
+              {/* <!--    djhrjb --> */}
+              <div>
+                <div noValidate autoComplete="off">
+                  <label>
+                    First Name
+                    <input
+                      placeholder="Full Name"
+                      className="firstName"
+                      onChange={(e) => this.setState({ fname: e.target.value })}
+                    />
+                  </label>
                 </div>
               </div>
-              <div className="LastName">
-                <div className="lname" noValidate autoComplete="off">
-                  <TextField
-                    id="lnameField"
-                    label="Last Name"
-                    variant="outlined"
-                    onChange={(e) => this.setState({ lname: e.target.value })}
-                  />
+
+              <div>
+                <div noValidate autoComplete="off">
+                  <label>
+                    Last Name
+                    <input
+                      placeholder="Last Name"
+                      className="lastName"
+                      onChange={(e) => this.setState({ lname: e.target.value })}
+                    ></input>
+                  </label>
                 </div>
               </div>
             </div>
-            <div className="Dob">
-              <label className="doblabel" htmlFor="dob">
-                Date of Birth:
-              </label>
-              <div className="dobmonth" noValidate autoComplete="off">
-                <TextField
-                  id="dobMonthField"
-                  label="mm"
-                  variant="outlined"
-                  onChange={(e) => this.setState({ dOBMonth: e.target.value })}
-                />
+            <div>
+              <div className="aod">
+                <label>
+                  Admission Date
+                  <input
+                    type="date"
+                    className="admissionDate"
+                    onChange={(e) =>
+                      this.setState({ admissionDate: e.target.value })
+                    }
+                  ></input>
+                </label>
               </div>
-              -
-              <div className="dobday" noValidate autoComplete="off">
+            </div>
+            <div>
+              {/* <label>Date of Birth:</label> */}
+              <div className="dob">
+                <label>
+                  Date of Birth
+                  <input
+                    type="date"
+                    className="dateOfBirth"
+                    onChange={(e) =>
+                      this.setState({ dateOfBirth: e.target.value })
+                    }
+                  ></input>
+                </label>
+              </div>
+              {/* - */}
+              {/* <div className="dobday" noValidate autoComplete="off">
                 <TextField
                   id="dobDayField"
                   label="dd"
@@ -95,37 +133,50 @@ class CreatePatient extends React.Component {
                   variant="outlined"
                   onChange={(e) => this.setState({ dOBYear: e.target.value })}
                 />
-              </div>
+              </div> */}
             </div>
-            <div className="hW">
-              <div className="Weight">
-                <div className="weightForm" noValidate autoComplete="off">
-                  <TextField
-                    id="weightField"
-                    label="Weight (lbs)"
-                    variant="outlined"
-                    onChange={(e) => this.setState({ weight: e.target.value })}
-                  />
+
+            {/* <div className="hW"> */}
+            <div className="cNames">
+              <div>
+                <div noValidate autoComplete="off">
+                  <label>
+                    Weight
+                    <input
+                      placeholder="Weight (lbs)"
+                      className="weightField"
+                      onChange={(e) =>
+                        this.setState({ weight: e.target.value })
+                      }
+                    />
+                  </label>
                 </div>
               </div>
-              <div className="Height">
-                <div className="heightForm" noValidate autoComplete="off">
-                  <TextField
-                    id="heightField"
-                    label="Height (m)"
-                    variant="outlined"
-                    onChange={(e) => this.setState({ height: e.target.value })}
-                  />
+
+              <div>
+                <div>
+                  <label>
+                    Height
+                    <input
+                      placeholder="Height (in)"
+                      className="heightField"
+                      onChange={(e) =>
+                        this.setState({ height: e.target.value })
+                      }
+                    />
+                  </label>
                 </div>
               </div>
             </div>
           </div>
+
           <div className="Buttons-createpatient">
             <div className="cancBut">
               <Button variant="contained" color="secondary">
                 Cancel
               </Button>
             </div>
+
             <div className="subBut">
               <Button variant="contained" color="primary" type="submit">
                 Submit
