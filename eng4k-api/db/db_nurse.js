93193e0c5a0339bb3fdc6fb30da170b107fb4693
wmaneesh <wmaneesh@outlook.com>
2021-02-26 14:54:01 -0500

    created a connection pool for sql

--- eng4k-api/db/db_nurse.js
+++ eng4k-api/db/db_nurse.js
@@ -2,24 +2,6 @@ const mysql = require("mysql");
 
 const pool = require("../dbPool.js");
 
-// const pool = mysql.createPool({
-//   connectionLimit: 1,
-//   password: "1335969a",
-//   user: "b43f5e001f1663",
-//   database: "heroku_c2367af0e7c5fa1",
-//   host: "us-cdbr-east-03.cleardb.com",
-//   port: 3306,
-// });
-
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   password: "password",
-//   user: "root",
-//   database: "capstonedb",
-//   host: "localhost",
-//   port: 3306,
-// });
-
 let nurseApiCalls = {};
 
 // nurseApiCalls.currentPatientList = (id) => {
--- eng4k-api/dbPool.js
+++ eng4k-api/dbPool.js
@@ -1,5 +1,14 @@
 var mysql = require("mysql");
 
+// const pool = mysql.createPool({
+//   connectionLimit: 1,
+//   password: "1335969a",
+//   user: "b43f5e001f1663",
+//   database: "heroku_c2367af0e7c5fa1",
+//   host: "us-cdbr-east-03.cleardb.com",
+//   port: 3306,
+// });
+
 const pool = mysql.createPool({
   connectionLimit: 10,
   password: "password",
